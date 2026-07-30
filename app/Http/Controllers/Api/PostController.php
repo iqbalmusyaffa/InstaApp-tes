2f57f2537172a39d06e03dbc6d46fd3bed93390a
iqbalmusyaffa <yusa82401@gmail.com>
2026-07-30 16:08:19 +0700

    update api controller post and route api.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
 
 class PostController extends Controller
 {
@@ -12,7 +16,15 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+         $posts = Post::with('user')
+            ->latest()
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data post berhasil diambil',
+            'data' => $posts
+        ]);
     }
 
     /**
@@ -20,7 +32,24 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         $request->validate([
+            'caption' => 'required|max:1000',
+            'image' => 'required|image|mimes:jpg,jpeg,png|max:2048'
+        ]);
+
+        $image = $request->file('image')->store('posts', 'public');
+
+        $post = Post::create([
+            'user_id' => Auth::id(),
+            'caption' => $request->caption,
+            'image' => $image,
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post berhasil dibuat',
+            'data' => $post
+        ], 201);
     }
 
     /**
@@ -28,7 +57,10 @@ class PostController extends Controller
      */
     public function show(string $id)
     {
-        //
+         return response()->json([
+            'success' => true,
+            'data' => $post->load('user')
+        ]);
     }
 
     /**
@@ -36,7 +68,35 @@ class PostController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+          if ($post->user_id != Auth::id()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 403);
+        }
+
+        $request->validate([
+            'caption' => 'required|max:1000',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
+        ]);
+
+        if ($request->hasFile('image')) {
+
+            Storage::disk('public')->delete($post->image);
+
+            $post->image = $request->file('image')
+                ->store('posts', 'public');
+        }
+
+        $post->caption = $request->caption;
+
+        $post->save();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post berhasil diupdate',
+            'data' => $post
+        ]);
     }
 
     /**
@@ -44,6 +104,20 @@ class PostController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+         if ($post->user_id != Auth::id()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Unauthorized'
+            ], 403);
+        }
+
+        Storage::disk('public')->delete($post->image);
+
+        $post->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post berhasil dihapus'
+        ]);
     }
 }
